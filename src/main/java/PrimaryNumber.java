2bd6e9f761a9e19a0d0ad1b5480229ed6c04bf27
osmandgn <oskan001@gmail.com>
2022-12-07 16:54:31 +0300

    V5

---
+++ src/main/java/PrimaryNumber.java
@@ -0,0 +1,47 @@
+public class PrimaryNumber {
+    public static void main(String[] args) {
+
+        isItPrimary(14); // Is it Primary ? -> It is not Primary
+        getPrimaryNumbers(5); // First 5 Primary Number -> 2 3 5 7 11
+
+
+
+    }
+    public static void isItPrimary(int x){
+        boolean result = true;
+        for (int i = 2; i < x; i++) {
+            if (x % i == 0){
+                result = false;
+            }
+        }
+        if (result){
+            System.out.println("It is Primary");
+        }else System.out.println("It is not Primary ");
+    }
+
+    public static void getPrimaryNumbers(int x){
+        int max = Integer.MAX_VALUE;
+        for (int i = 2; i < max; i++) {
+            boolean result = true;
+            for (int j = 2; j < i; j++){
+                if (i % j == 0){
+                    result = false;
+                }
+            }
+            if (result){
+                System.out.print(i + " ");
+                x--;
+                if (x == 0){
+                    break;
+                }
+            }
+
+        }
+
+
+
+    }
+
+
+
+}
