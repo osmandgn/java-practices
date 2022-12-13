e47068eb06fe06aabd9af2756d17b6abba487674
osmandgn <oskan001@gmail.com>
2022-12-13 17:26:26 +0300

    V4

--- src/main/java/StringAndCharacterCounter.java
+++ src/main/java/StringAndCharacterCounter.java
@@ -11,13 +11,13 @@ public class StringAndCharacterCounter {
 
     }
 
-    public static void numOfChar(String str){
+    public static void numOfChar(String str) {
         HashMap<Character, Integer> hashMap = new HashMap<>();
         char[] charArray = str.toCharArray();
-        for (char c: charArray){
-            if (hashMap.containsKey(c)){
-                hashMap.put(c, hashMap.get(c)+1);
-            }else{
+        for (char c : charArray) {
+            if (hashMap.containsKey(c)) {
+                hashMap.put(c, hashMap.get(c) + 1);
+            } else {
                 hashMap.put(c, 1);
             }
         }
@@ -25,20 +25,20 @@ public class StringAndCharacterCounter {
 
     }
 
-    public static void kelimeBul(String str){
+    public static void kelimeBul(String str) {
         String[] strArray = str.split(" ");
         HashMap<String, Integer> map = new HashMap<>();
-        for (String w : strArray){
-            if (map.containsKey(w)){
-                map.put(w, map.get(w)+1);
-            }else{
+        for (String w : strArray) {
+            if (map.containsKey(w)) {
+                map.put(w, map.get(w) + 1);
+            } else {
                 map.put(w, 1);
             }
         }
 
         Set<String> mySet = map.keySet();
-        for (String w: mySet){
-            if (map.get(w) > 1){
+        for (String w : mySet) {
+            if (map.get(w) > 1) {
                 System.out.println(w + " : " + map.get(w));
             }
 
@@ -48,28 +48,6 @@ public class StringAndCharacterCounter {
     }
 
 
-    public static void wordCounter(String str){
-        HashMap<String, Integer> myMap = new HashMap<>();
-        String[] strArray = str.split(" ");
-        for (String w: strArray){
-            if (myMap.containsKey(w)){
-                myMap.put(w, myMap.get(w) + 1);
-            }else {
-                myMap.put(w, 1);
-            }
-        }
-
-        for (Map.Entry<String, Integer> q: myMap.entrySet()){
-            if (q.getValue() > 1){
-                System.out.println(q);
-            }
-        }
-
-
-
-
-
-    }
 
 
 
