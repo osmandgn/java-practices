2cd88b63e64f6d933d0b7c9465a67fb84f7adf6a
osmandgn <oskan001@gmail.com>
2022-12-13 19:02:05 +0300

    V4

---
+++ src/main/java/PascalTriangle.java
@@ -0,0 +1,27 @@
+public class PascalTriangle {
+    public static void main(String[] args) {
+
+        pascalTriangle(5);
+
+
+    }
+
+    public static void pascalTriangle(int g)
+    {
+        for (int s = 1; s <= g; s++) {
+            for (int b = 0; b <= g - s; b++) {
+                System.out.print(" ");
+            }
+            int k = 1;
+            for (int i = 1; i <= s; i++) {
+                System.out.print(k + " ");
+                k = k * (s - i) / i;
+
+            }
+            System.out.println();
+        }
+    }
+
+
+}
+
