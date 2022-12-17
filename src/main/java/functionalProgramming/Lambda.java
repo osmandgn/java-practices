5c4aa6cda311490f2d017c70df9f74a3df593f90
osmandgn <oskan001@gmail.com>
2022-12-17 16:56:24 +0300

    V4

--- src/main/java/functionalProgramming/Lambda.java
+++ src/main/java/functionalProgramming/Lambda.java
@@ -72,6 +72,20 @@ public class Lambda {
 
     }
 
+    public static Map<String, List<Person>> groupByNationaly(List<Person> people) {
+        Map<String, List<Person>> map = new HashMap<>();
+        for (Person person : people) {
+            if (!map.containsKey(person.getNationality())) {
+                map.put(person.getNationality(), new ArrayList<>());
+            }
+            map.get(person.getNationality()).add(person);
+        }
+        return map;
+    }
+
+    public static Map<String, List<Person>> groupByNationality(List<Person> people) {
+        return people.stream().collect(Collectors.groupingBy(Person::getNationality, Collectors.toList()));
+    }
 
 
 
