0374285971af4b73637172533312650eac318690
osmandgn <oskan001@gmail.com>
2022-12-18 11:01:24 +0300

    V6

---
+++ src/main/java/StringContainsVowels.java
@@ -0,0 +1,13 @@
+public class StringContainsVowels {
+
+    public static void main(String[] args) {
+        String str1 = "Movie";
+        System.out.println(isStringContainsVowels(str1)); // true
+        String str2 = "TV";
+        System.out.println(isStringContainsVowels(str2)); // false
+
+    }
+    public static boolean isStringContainsVowels(String str){
+        return str.toLowerCase().matches(".*[aeiou].*");
+    }
+}
