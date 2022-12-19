5503265752c3b70bfacd11653228af8aa29aec55
osmandgn <oskan001@gmail.com>
2022-12-19 18:16:07 +0300

    V4

--- src/main/java/AnonymousThread.java
+++ src/main/java/AnonymousThread.java
@@ -1,3 +1,32 @@
 public class AnonymousThread {
+    public static void main(String[] args) {
 
+        for (int i=1; i <= 5; i++) {
+            System.out.println("run() method: " + i + " " + Thread.currentThread().getName());
+        }
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i=1; i <= 5; i++) {
+                    System.out.println("run() method: " + i + " " + Thread.currentThread().getName());
+                }
+            }
+        });
+        thread.start();
+
+
+
+
+        new Thread(() -> {
+            Thread.currentThread().setName("Isim");
+            for (int i=1; i <= 5; i++) {
+                System.out.println("Lambda : " + i + " " + Thread.currentThread().getName());
+            }
+        }).start();
+
+
+
+
+    }
 }
