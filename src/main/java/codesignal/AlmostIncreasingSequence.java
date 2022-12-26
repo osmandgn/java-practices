3e69dc14a851e4ef7494c9a1a218f9be7b8ceaad
osmandgn <oskan001@gmail.com>
2022-12-26 18:11:03 +0300

    V4

--- src/main/java/codesignal/AlmostIncreasingSequence.java
+++ src/main/java/codesignal/AlmostIncreasingSequence.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class AlmostIncreasingSequence {
     public static void main(String[] args) {
         // [1, 3, 2, 1]
-        int[] sequence = {1, 2, 5, 3, 5};
+        int[] sequence = {1, 3, 2, 1};
         System.out.println(solution(sequence));
 
 
@@ -14,30 +14,15 @@ public class AlmostIncreasingSequence {
 
 
     static boolean solution(int[] sequence) {
-        int counter = 0;
-
-        for (int i = 0; i < sequence.length - 2 && counter <= 2; i ++) {
-            int a = sequence[i];
-            int b = sequence[i+1];
-            int c = sequence[i+2];
-
-            if (a >= b) {
-                counter++;
-                sequence[i] = b -1;
-            }
-
-            if (b >= c){
-                counter++;
-
-                if (a == c) {
-                    sequence[i+2] = b +1;
-                } else {
-                    sequence[i+1] = a;
-                }
-            }
+        int count1 = 0, count2 = 0;
+        for (int i = 0; i < sequence.length - 1; i++) {
+            if (sequence[i] >= sequence[i + 1]) count1++;
         }
 
-        return counter <= 1;
+        for (int i = 0; i < sequence.length - 2; i++) {
+            if (sequence[i] >= sequence[i + 2]) count2++;
+        }
+        return (count1 <= 1) && (count2 <= 1);
 
     }
 }
