b29bc2320d227d7cd8cfa247bb32fcf09601c9ea
osmandgn <oskan001@gmail.com>
2022-12-31 17:36:42 +0300

    V4

--- src/main/java/codesignal/AlmostIncreasingSequence.java
+++ src/main/java/codesignal/AlmostIncreasingSequence.java
@@ -6,8 +6,9 @@ import java.util.List;
 public class AlmostIncreasingSequence {
     public static void main(String[] args) {
         // [1, 3, 2, 1]
-        int[] sequence = {1, 3, 2, 1};
+        int[] sequence = {1, 2, 5, 3, 5};
         System.out.println(solution(sequence));
+        //[1, 2, 5, 3, 5]
 
 
     }
@@ -15,12 +16,16 @@ public class AlmostIncreasingSequence {
 
     static boolean solution(int[] sequence) {
         int count1 = 0, count2 = 0;
+
         for (int i = 0; i < sequence.length - 1; i++) {
             if (sequence[i] >= sequence[i + 1]) count1++;
+            System.out.println(i + " dogunsu c1 " + count1);
         }
 
         for (int i = 0; i < sequence.length - 2; i++) {
             if (sequence[i] >= sequence[i + 2]) count2++;
+            System.out.println(i + " dogunsu c2 " + count2);
+
         }
         return (count1 <= 1) && (count2 <= 1);
 
---
+++ src/main/java/interviewquestions/Asterisk.java
@@ -0,0 +1,25 @@
+package interviewquestions;
+
+import java.util.Scanner;
+
+public class Asterisk {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int i = scanner.nextInt();
+        String x = "*";
+        String y = " ".repeat(i);
+        for (int j = 0; j < i; j++) {
+            System.out.print(y.substring(j));
+            System.out.println(x);
+            x = x + "**";
+        }
+        System.out.println(x);
+        for (int j = i; j > 0; j--) {
+            x = x.substring(0, x.length() - 2);
+            System.out.print(y.substring((j - 1)));
+            System.out.println(x);
+
+        }
+    }
+}
+
