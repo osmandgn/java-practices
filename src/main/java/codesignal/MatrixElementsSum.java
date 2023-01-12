08ccd0a275de665171a68e9f63228a0e4f82dd35
osmandgn <oskan001@gmail.com>
2023-01-12 20:58:19 +0300

    V2

--- src/main/java/codesignal/MatrixElementsSum.java
+++ src/main/java/codesignal/MatrixElementsSum.java
@@ -19,17 +19,7 @@ public class MatrixElementsSum {
 
     }
 
-    int solution(int[][] matrix) {
-        int sum = 0;
-        for (int i = 0; i < matrix[0].length; i++) {
-            for (int j = 0; j < matrix.length; j++) {
-                sum += matrix[j][i];
-                //System.out.println(matrix[j][i]);
-                if (matrix[j][i] == 0)
-                    break;
-            }
-        }
-        return sum;
+
 
 
     }
